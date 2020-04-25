592700dcfe663f9cbfed37e97656c489b348b0c1
abu saleh matul <saleh.matul@gmail.com>
2020-04-25 21:18:33 +0600

    ticket

---
+++ database/migrations/2020_04_25_142231_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('barcode')->nullable();
+            $table->text('description')->default('system_ticket');
+            $table->unsignedBigInteger('opner_user_id');
+            $table->enum('status', ['pending', 'solved', 'canceled'])->default('pending');
+            $table->timestamps();
+            $table->foreign('opner_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
